b398e835107611fed0f29a31ce5c30f93e20ffd6
Valentas Klimas <valentasklimas@gmail.com>
2021-01-27 20:24:53 +0000

    photo model

--- SavePhotoToFolderAPI/Models/Photo.cs
+++ SavePhotoToFolderAPI/Models/Photo.cs
@@ -7,5 +7,10 @@ namespace SavePhotoToFolderAPI.Models
 {
     public class Photo
     {
+        public string UserName { get; set; }
+        public string PhotoID { get; set; }
+        public bool Resized { get; set; }
+        public int PhotoWidth { get; set; }
+        public int PhotoHeight { get; set; } // nu kas dbr ? 
     }
 }
